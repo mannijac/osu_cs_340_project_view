f66e19d6f528b4ae29dc0d3ff44d9ada7538bf51
Allison Land <landallison@gmail.com>
2022-02-10 17:27:25 -0500

    updated imports

--- src/pages/Owners.js
+++ src/pages/Owners.js
@@ -1,1 +1,5 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import App from '../App';
+
 export default OwnersPage;
--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,4 +1,6 @@
-
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import App from '../App';
 
 
 export default UsersPage;
