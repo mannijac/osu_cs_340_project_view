8a7620a240691100cf3dfda4b389c478dcc3201e
mannijac <mannijac@oregonstate.edu>
2022-03-08 09:14:05 -0500

    Add axios support

--- src/components/InsertForm.js
+++ src/components/InsertForm.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import axios from axios;
 import { useState } from 'react';
 import labels from '../data/labels';
 import apiURL from '../data/apiURL';
 import Input from './Input';
 
 export default function InsertForm(props) {
-    const data = {};
-    data.table_name = props.data.name;
-    data.keys = props.data.inputs;
+    const formData = new FormData();
+    formData. append(table_name, props.data.name);
+    formData.append( keys, props.data.inputs);
     const [values, setValues] = useState({});
     
     const handleInput = ({ target: {name, value}}) => {
@@ -16,17 +17,13 @@ export default function InsertForm(props) {
     
     async function handleSubmit(e) {
         e.preventDefault();  
-        data.values = values;
+        formData.append(values, values);
         console.log(data);
-        let response = await fetch(apiURL, {  
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
+        axios.post(apiURL, {formData})
         })
-            .then(response => response.json())
-            .then(data => console.log(data))
+            .then(response => {
+                console.log(response)
+            })
             .catch(error => console.log(error));  
     }
     
