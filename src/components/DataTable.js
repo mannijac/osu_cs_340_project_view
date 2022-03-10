2ef1bac1f38c1a3ab485dd232f224e836ebbab89
mannijac <mannijac@oregonstate.edu>
2022-03-10 08:27:42 -0500

    begin request for table data

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import TableHead from '../components/TableHead'
+import axios from 'axios';
+import TableHead from '../components/TableHead';
+import apiURL from '../data/apiURL';
 
-export default function DataTable(props) {
-    let column = props.data
+export default async function DataTable(props) {
+    let dataTable = props.data
+
+    const results = await axios.get(apiURL, {params: {
+        headers: {'Content-Type': 'application/json'}
+    }}); // This is not finished
+
     const actionColumns = ['Collections', 'Wishes', 'Delete']
-    let columnLabels = column.columnLabels.map((val) => val); //Clone column labels
-    if (column.name === 'users' || column.name === 'games') {
+    let columnLabels = dataTable.columnLabels.map((val) => val); //Clone column labels
+    if (dataTable.name === 'users' || dataTable.name === 'games') {
         columnLabels = columnLabels.concat(actionColumns);
     } else {
         columnLabels = columnLabels.concat(['Delete']);
     }
+
+
     return (
         <table>
                 <TableHead data={columnLabels}></TableHead>
