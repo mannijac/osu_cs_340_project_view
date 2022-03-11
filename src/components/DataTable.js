09fc44ec7c869c107d7e63a08f19d338a98d2636
mannijac <mannijac@oregonstate.edu>
2022-03-11 00:37:19 -0500

    debug undefined value

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -4,17 +4,17 @@ import TableHead from '../components/TableHead';
 import TableRow from '../components/TableRow';
 import apiURL from '../data/apiURL';
 
-async function getData(table_name) {
-    let response = await axios.get(apiURL, {params: {
-        headers: {'Content-Type': 'application/json'},
-        table_name: table_name
-    }});
-    return response;
-}
 
 export default function DataTable(props) {
-    const response = getData(props.data.name);
     const dataTable = props.data;
+    let response = [];
+    async function getData(table_name) {
+        response = await axios.get(apiURL, {params: {
+            headers: {'Content-Type': 'application/json'},
+            table_name: table_name
+        }});
+    }
+    getData(dataTable.name);
     console.log(response.data);
     const actionColumns = ['Collections', 'Wishes', 'Delete']
     let columnLabels = dataTable.columnLabels.map((val) => val); //Clone column labels
