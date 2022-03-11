e757f63e7cc1e816e1c4876b9e274b70480efa2f
mannijac <mannijac@oregonstate.edu>
2022-03-10 23:19:23 -0500

    extract data from response

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -9,7 +9,7 @@ async function getData(table_name) {
         headers: {'Content-Type': 'application/json'},
         table_name: table_name
     }});
-    console.log('Data: ', response, typeof(response));
+    console.log('Data: ', response.data, typeof(response.data));
     return response.data;
 }
 
