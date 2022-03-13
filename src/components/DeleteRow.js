e12f3b42786aafe3f2b7568f15b7233eb96d521f
mannijac <mannijac@oregonstate.edu>
2022-03-13 17:32:19 -0400

    fix delete request

--- src/components/DeleteRow.js
+++ src/components/DeleteRow.js
@@ -3,10 +3,14 @@ import react from "react";
 import {MdDelete} from 'react-icons/md';
 import apiURL from "../data/apiURL";
 
-async function handleClick(props) {
-    const name = props.name;
-    const id = props.rowID;
-    await axios.delete(apiURL, {'table_name': name, 'rowID': id});
+function handleClick(props) {
+    
+    async function sendDelete(props) {
+        const name = props.name;
+        const id = props.rowID;
+        await axios.delete(apiURL, {'table_name': name, 'rowID': id});
+    }
+    sendDelete(props);
 }
 
 export default function DeleteRow(props) {
