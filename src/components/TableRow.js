8de21ce12bef3cbab8e580f34dda40cd752a0b3d
mannijac <mannijac@oregonstate.edu>
2022-03-13 18:39:18 -0400

    bug hunting

--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -4,14 +4,8 @@ import DeleteRow from './DeleteRow';
 
 export default function TableRow(props) {
     const tableRow = props.data;
-    let row_id = null;
+    let row_id = tableRow[0];
 
-    if (tableRow['user_id'] != null) {
-        row_id = tableRow['user_id'];
-    } else if (tableRow['game_id'] != null) {
-        row_id = tableRow['game_id'];
-    }
-    
     return(
         <tr>
             {tableRow.map((value, i) => <TableValue data={value}/>)}
