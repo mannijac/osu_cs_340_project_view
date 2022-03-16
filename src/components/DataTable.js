5d2b30474c62c3fbc2710f939e48f4532de2b79b
mannijac <mannijac@oregonstate.edu>
2022-03-16 07:19:43 -0400

    restructure useeffect to get table data once per update

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -6,23 +6,10 @@ import TableRow from '../components/TableRow';
 import apiURL from '../data/apiURL';
 import { useEffect } from 'react/cjs/react.production.min';
 
-async function getData(table_name) {
-    const response = await axios.get(apiURL, {params: {
-        headers: {'Content-Type': 'application/json'},
-        table_name: table_name
-    }});
-    console.log(response.data);
-    setData(response.data);
-}
-
 export default function DataTable(props) {
     const dataTable = props.data;
-    const [data, setData] = useState([]);
-    
-    useEffect(() => {
-        getData(dataTable.name);
-    });
-    
+    const [data, setData] = useState([]);    
+    // Build table columns
     const actionColumns = ['Delete'] // Tac on column for delete icon
     let columnLabels = dataTable.columnLabels.map((val) => val); //Clone column labels
     if (dataTable.name === 'users' || dataTable.name === 'games') {
@@ -30,6 +17,21 @@ export default function DataTable(props) {
     } else {
         columnLabels = columnLabels.concat(['Delete']);
     }
+    // Get table data
+    useEffect(() => {
+        async function getData(table_name) {
+            const response = await axios.get(apiURL, {params: {
+                headers: {'Content-Type': 'application/json'},
+                table_name: table_name
+            }});
+            console.log(response.data);
+            setData(response.data);
+        }
+        
+        getData(dataTable.name);
+    });
+
+
     return (
         <table>
             <TableHead data={columnLabels}></TableHead>
