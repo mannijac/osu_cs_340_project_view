124f8f35e5275eaf97f409d2910b317d334869ce
mannijac <mannijac@oregonstate.edu>
2022-03-17 18:53:33 -0400

    bug fix

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,6 +3,8 @@ import react from "react";
 export default function Options(props) {
 
     return(
+        <div>
         {props.data.map((column, i) => <option key={i} value={column}>{column}</option>)}
+        </div>
     )
 }
