789a05b9df2310011ddba2a94a2fa18b0820abdc
mannijac <mannijac@oregonstate.edu>
2022-03-18 02:07:37 -0400

    syntax

--- src/components/DeleteRow.js
+++ src/components/DeleteRow.js
@@ -4,7 +4,7 @@ import {MdDelete} from 'react-icons/md';
 import apiURL from "../data/apiURL";
 
 
-function async sendDelete(tableData) {
+async function sendDelete(tableData) {
     res = await axios.delete(apiURL, {data: {id: tableData.id, table_name: tableData.table_name}});
     alert(res['success'])
     if (res['error'] === null) {
