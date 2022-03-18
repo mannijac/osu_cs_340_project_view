73816e52fc88af768ad03bb5b0ef92880aa9918e
mannijac <mannijac@oregonstate.edu>
2022-03-18 01:50:03 -0400

    add alerts for delete action and page reload for delete and update

--- src/components/DeleteRow.js
+++ src/components/DeleteRow.js
@@ -5,7 +5,9 @@ import apiURL from "../data/apiURL";
 
 
 function sendDelete(tableData) {
-    axios.delete(apiURL, {data: {id: tableData.id, table_name: tableData.table_name}});
+    res = axios.delete(apiURL, {data: {id: tableData.id, table_name: tableData.table_name}});
+    alert(res)
+    window.location.reload(false)
 }
 
 export default function DeleteRow(props) {
