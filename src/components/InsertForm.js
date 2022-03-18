83b74f05fd73666659b8889077a97494369f1cba
mannijac <mannijac@oregonstate.edu>
2022-03-18 02:34:03 -0400

    get response string

--- src/components/InsertForm.js
+++ src/components/InsertForm.js
@@ -23,12 +23,12 @@ export default function InsertForm(props) {
         })
             .then(response => {
                 console.log(response)
-                alert(String(response))
+                alert(response['success'])
                 window.location.reload(false)
             })
             .catch(error => {
                 console.log(error)
-                alert(String(error))
+                alert(error)
             });  
     }
     return (
